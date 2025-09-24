b913c7d924a4b4487d73d68438dc574b46274b0d
lainyxx <straightening3@gmail.com>
2025-09-24 19:27:26 +0900

    グーグルマップをメイン画面に追加

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
   constructor() {}
+
+  map!: GoogleMap;
+
+  async ngAfterViewInit() {
+    this.map = await GoogleMap.create({
+      id: 'map', // HTMLで指定したID
+      element: document.getElementById('map')!,
+      apiKey: 'AIzaSyC-MAdmwXw9gGTwAxGMDjD1SNgKONy9sB8',
+      config: {
+        center: {
+          lat: 35.6804,  // 東京駅
+          lng: 139.7690,
+        },
+        zoom: 12,
+      },
+    });
+
+    // マーカーを追加
+    await this.map.addMarker({
+      coordinate: { lat: 35.6804, lng: 139.7690 },
+      title: '東京駅',
+    });
+  }
 }
