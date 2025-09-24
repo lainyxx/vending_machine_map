ad02bf378d5b21cf6459bbc83f3793839cb3a167
lainyxx <straightening3@gmail.com>
2025-09-24 23:27:15 +0900

    マップを表示するところまで実装
    googlemapを表示できるようになった。
    apikeyを環境変数で管理するように設定。
    gradle.propertiesをgitignoreに追加。

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,36 +1,41 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { GoogleMap } from '@capacitor/google-maps';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { environment } from '../../environments/environment';
+
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab1Page implements AfterViewInit {
-  constructor() {}
 
   map!: GoogleMap;
 
+  constructor() {}
+
   async ngAfterViewInit() {
+    // マップ作成
     this.map = await GoogleMap.create({
-      id: 'map', // HTMLで指定したID
+      id: 'map',
       element: document.getElementById('map')!,
-      apiKey: 'AIzaSyC-MAdmwXw9gGTwAxGMDjD1SNgKONy9sB8',
+      apiKey: environment.googleMapsApiKey,
       config: {
-        center: {
-          lat: 35.6804,  // 東京駅
-          lng: 139.7690,
-        },
+        center: { lat: 35.6804, lng: 139.7690 }, // 東京駅
         zoom: 12,
       },
     });
 
-    // マーカーを追加
+    // マーカー追加（plain objectで座標指定）
     await this.map.addMarker({
       coordinate: { lat: 35.6804, lng: 139.7690 },
       title: '東京駅',
+      snippet: 'ここから出発'
     });
+
   }
 }
